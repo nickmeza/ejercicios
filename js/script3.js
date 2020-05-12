8cf2cc1e686a9a186051ad2e1a87f4400418ad61
Nick Carlos Mesias Rivera <nickmeza@upeu.edu.pe>
2020-05-12 00:36:32 -0500

    nick

---
+++ js/script3.js
@@ -0,0 +1,31 @@
+var nombre =prompt("Nombre: " )
+var nega=[]
+var edad =prompt("Edad: " )
+var salario =prompt("Salario: " )
+function calcularedad() {
+    if (edad<=18) {
+        return salario="no tiene edad para trabajar"
+    } else {
+        if (edad>=19 && edad<=50) {
+            aumento = salario*0.05
+            salario=parseInt(salario)+parseInt(aumento)
+            console.log(aumento)
+            return salario
+        } else {
+            if (edad>=51 && edad<=60) {
+                aumento = salario*0.10
+                salario=parseInt(salario)+parseInt(aumento)
+                console.log(aumento)
+                return salario
+            } else if (edad>=60) {
+                aumento = salario*0.15
+                salario=parseInt(salario)+parseInt(aumento)
+                console.log(aumento)
+                return salario
+            }
+        }
+    }
+    
+}
+
+alert(calcularedad())
